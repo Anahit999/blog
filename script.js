d2e8e7c01d005f01b107d97f1f047ea71a0cdb70
Anahit999 <anahityolchyan7@gmail.com>
2025-10-27 14:35:19 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+let z= 8.437;
+let z1=z.toFixed(2);
+console.log(z1);//1in xndir
+let fruit=["Banaan", "Orange", "Apple", "Mango"]
+let fruit1 = fruit.push("kiwi");
+console.log(fruit);//12rd xndir
+let text = "Bye John";
+let result = text.toUpperCase();
+console.log(result)//5rd xndir
+let b="Good bye";
+console.log(b);
+console.log(b.replace(/ /g,"_"));//7rd xndir
